a11e0613de7d6d34c7014202b708b972f427b5b5
mohmdrezaei <mhwmdrz9@gmail.com>
2024-10-17 23:51:00 +0330

    Handling confirmPassword and optimizing registration codes

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,31 +2,63 @@ import React, { useState } from "react";
 import { registerUser } from "../services/auth";
 
 function RegisterPage() {
-  const [username , setUsername] =useState ("")
-  const [password, setPassword] = useState("");
-  const submitHandler =async (e) => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    const {response , error} = await registerUser(username, password)
-     console.log({response , error})
+    const { username, password, confirmPassword } = formData;
+
+    if (password !== confirmPassword) {
+      alert("گذرواژه ها مطابقت ندارند. لطفا دوباره امتحان کنید.");
+      return;
+    }
+
+    const { response, error } = await registerUser(username, password);
+    if(response){
+      console.log("ثبت نام با موفقیت انجام شد:", response);
+    }
+    if(error){
+      console.log("ثبت نام با مشکل ربرو شد!:", error.response.data.message);
+    }
   };
 
   return (
     <div>
       <h1>فرم ثبت نام</h1>
       <form onSubmit={submitHandler}>
-        <input
+      <input
           type="text"
+          name="username"
           placeholder="نام کاربری"
-          value={username}
-          onChange={(e)=>setUsername(e.target.value)}
+          value={formData.username}
+          onChange={changeHandler}
         />
         <input
           type="password"
+          name="password"
           placeholder="رمز عبور"
-          value={password}
-          onChange={(e)=>setPassword(e.target.value)}
+          value={formData.password}
+          onChange={changeHandler} 
         />
-        {/* <input type="password" placeholder='تکرار رمز عبور'/> */}
+        <input
+          type="password"
+          name="confirmPassword" 
+          placeholder="تکرار رمز عبور"
+          value={formData.confirmPassword}
+          onChange={changeHandler}
+        />
         <button type="submit">ثبت نام</button>
       </form>
       <a href="#">حساب کاربری دارید؟</a>
