dfddf3b0948791d2fc522d8cf3f6cc7a3399e35e
mohmdrezaei <mhwmdrz9@gmail.com>
2024-10-28 19:53:56 +0330

    Separating the routes from the apps

--- src/App.jsx
+++ src/App.jsx
@@ -10,58 +10,20 @@ import PageNotFound from "pages/404";
 import TanstackQueryProvider from "provider/TanstackQueryProvider";
 import { getCookie } from "utils/cookie";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import ProtectedRoutes from "router/ProtectedRoutes";
-import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import Router from "router/Router";
+
+
 
 
 
 
 function App() {
   
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-    confirmPassword: "",
-  });
-  const token = getCookie("token");
+ 
   
   return (
     <TanstackQueryProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/register"
-            element={
-              !token ? (
-                <RegisterPage formData={formData} setFormData={setFormData} />
-              ) : (
-                <Navigate to="/products" />
-              )
-              
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              !token ? (
-                <LoginPage formData={formData} setFormData={setFormData} />
-              ) : (
-                <Navigate to="/products" />
-              )
-            }
-          />
-          <Route path="/" element={<Navigate to="/login" />} />
-         <Route  element={<ProtectedRoutes/>}>
-         <Route path="/products" element={ <ProductsPage/>}/>
-          </Route>
-          <Route path="*" element={ <PageNotFound/>}/>
-        </Routes>
-      </BrowserRouter>
+    <Router />
       <ToastContainer
         position="top-center"
         autoClose={3000}
---
+++ src/router/Router.jsx
@@ -0,0 +1,50 @@
+import ProtectedRoutes from "router/ProtectedRoutes";
+import { useState } from "react";
+import { getCookie } from "utils/cookie";
+import RegisterPage from "pages/RegisterPage";
+import LoginPage from "pages/LoginPage";
+import ProductsPage from "pages/ProductsPage";
+import PageNotFound from "pages/404";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+function Router() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const token = getCookie("token");
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/register"
+          element={
+            !token ? (
+              <RegisterPage formData={formData} setFormData={setFormData} />
+            ) : (
+              <Navigate to="/products" />
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            !token ? (
+              <LoginPage formData={formData} setFormData={setFormData} />
+            ) : (
+              <Navigate to="/products" />
+            )
+          }
+        />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/products" element={<ProductsPage />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
